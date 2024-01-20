7b90bc59425743efd955d84e78a5d1ba90434d72
george-pench <penchev.georgii@gmail.com>
2024-01-20 14:20:08 +0200

    Add /logs to .gitignore. Create a constant for ConnectionString. Add migrations files for changes on identity.

--- src/Data/E-Games.Data/Data/ApplicationDbContext.cs
+++ src/Data/E-Games.Data/Data/ApplicationDbContext.cs
@@ -7,16 +7,16 @@ namespace E_Games.Data.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
+        private const string ConnectionString = @"Server=DESKTOP-58V9NB9\\SQLEXPRESS;Database=E-Games;Trusted_Connection=True;Encrypt=false;TrustServerCertificate=true;MultipleActiveResultSets=true";
+
         public ApplicationDbContext() { }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+            : base(options) { }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=DESKTOP-58V9NB9\\SQLEXPRESS;Database=E-Games;Trusted_Connection=True;Encrypt=false;TrustServerCertificate=true;MultipleActiveResultSets=true");
+            optionsBuilder.UseSqlServer(ConnectionString);
 
             base.OnConfiguring(optionsBuilder);
         }
